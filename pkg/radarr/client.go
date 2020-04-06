f149a519d8a17a1b1dc95ae82188699fd0a55931
Devin Buhl <devin.kray@gmail.com>
2020-04-06 10:55:44 -0400

    remove apiendpoint var

--- pkg/radarr/client.go
+++ pkg/radarr/client.go
@@ -10,10 +10,6 @@ import (
 	"github.com/onedr0p/radarr-exporter/pkg/metrics"
 )
 
-var (
-	apiUrlPattern = "%s/api/%s"
-)
-
 // Client struct is a Radarr client to request an instance of a Radarr
 type Client struct {
 	httpClient http.Client
@@ -43,7 +39,7 @@ func (c *Client) Scrape() {
 
 		// System Status
 		status := SystemStatus{}
-		if err := c.apiRequest(fmt.Sprintf(apiUrlPattern, c.hostname, "system/status"), &status); err != nil {
+		if err := c.apiRequest(fmt.Sprintf("%s/api/%s", c.hostname, "system/status"), &status); err != nil {
 			metrics.Status.WithLabelValues(c.hostname).Set(0.0)
 			return
 		} else if (SystemStatus{}) == status {
@@ -64,7 +60,7 @@ func (c *Client) Scrape() {
 			moviesQualities   = map[string]int{}
 		)
 		movies := Movie{}
-		c.apiRequest(fmt.Sprintf(apiUrlPattern, c.hostname, "movie"), &movies)
+		c.apiRequest(fmt.Sprintf("%s/api/%s", c.hostname, "movie"), &movies)
 		for _, s := range movies {
 			if s.HasFile == true {
 				moviesDownloaded++
@@ -94,18 +90,18 @@ func (c *Client) Scrape() {
 
 		// History
 		history := History{}
-		c.apiRequest(fmt.Sprintf(apiUrlPattern, c.hostname, "history"), &history)
+		c.apiRequest(fmt.Sprintf("%s/api/%s", c.hostname, "history"), &history)
 		metrics.History.WithLabelValues(c.hostname).Set(float64(history.TotalRecords))
 
 		// Wanted
 		wanted := WantedMissing{}
-		c.apiRequest(fmt.Sprintf(apiUrlPattern, c.hostname, "wanted/missing"), &wanted)
+		c.apiRequest(fmt.Sprintf("%s/api/%s", c.hostname, "wanted/missing"), &wanted)
 		metrics.Wanted.WithLabelValues(c.hostname).Set(float64(wanted.TotalRecords))
 
 		// Queue by Status
 		var queueStatus = map[string]int{}
 		queue := Queue{}
-		c.apiRequest(fmt.Sprintf(apiUrlPattern, c.hostname, "queue"), &queue)
+		c.apiRequest(fmt.Sprintf("%s/api/%s", c.hostname, "queue"), &queue)
 		for _, s := range queue {
 			if s.TrackedDownloadStatus != "" {
 				queueStatus[s.TrackedDownloadStatus]++
@@ -117,14 +113,14 @@ func (c *Client) Scrape() {
 
 		// Root Folder
 		rootFolders := RootFolder{}
-		c.apiRequest(fmt.Sprintf(apiUrlPattern, c.hostname, "rootfolder"), &rootFolders)
+		c.apiRequest(fmt.Sprintf("%s/api/%s", c.hostname, "rootfolder"), &rootFolders)
 		for _, rootFolder := range rootFolders {
 			metrics.RootFolder.WithLabelValues(c.hostname, rootFolder.Path).Set(float64(rootFolder.FreeSpace))
 		}
 
 		// Health Issues
 		health := Health{}
-		c.apiRequest(fmt.Sprintf(apiUrlPattern, c.hostname, "health"), &health)
+		c.apiRequest(fmt.Sprintf("%s/api/%s", c.hostname, "health"), &health)
 		for _, h := range health {
 			metrics.Health.WithLabelValues(c.hostname, h.Type, h.Message, h.WikiURL).Set(float64(1))
 		}
